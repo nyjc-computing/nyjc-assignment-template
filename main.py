335f9e14c14a537668e2392c3a451bbee26124d4
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-01-05 15:31:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-# Built-in imports
+num = input('Enter a number (decimal or integer): ')
+# type your code here
 
+
+
+# do not change any code beyond this point
+print('The number', num, 'has', sf, 'significant figures.')
