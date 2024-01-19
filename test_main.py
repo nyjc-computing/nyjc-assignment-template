6061f91693cf0c1d83a3c29acd4237b7c95e0bb6
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-01-19 10:26:03 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,4 +5,8 @@ from autograding.case import InOut, FuncCall
 
 
 if __name__ == '__main__':
+    import os
+    if not os.listdir("autograding"):
+        import subprocess
+        subprocess.run(["git", "submodule", "update", "--init"])
     unittest.main()
