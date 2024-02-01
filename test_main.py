fd669ded7ca66393f5e93ddefd3b1690c1101625
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2024-02-01 13:51:25 +0800

    Improve autograding auto-import

--- test_main.py
+++ test_main.py
@@ -1,12 +1,26 @@
+import sys
 import unittest
 
-from autograding import TestInputOutput, TestFunction
-from autograding.case import InOut, FuncCall
+# autograding submodule might not be successfully pulled on init
+# if unsuccessful, we have to pull it manually
+# limit number of retries
+MAX_RETRIES = 5
+retries = 0
+while "autograding" not in sys.modules:
+    try:
+        import autograding
+        from autograding.case import FuncCall, InOut
+    except ImportError:
+        import subprocess
+        subprocess.run(["git", "submodule", "update", "--init"])
+        retries += 1
+    if retries >= MAX_RETRIES:
+        sys.exit("[import autograding] Too many retries, exiting")
+
+
+# TestMyCode(autograding.TestInputOutput):
+# TestMyFunction(autograding.TestFunction):
 
 
 if __name__ == '__main__':
-    import os
-    if not os.listdir("autograding"):
-        import subprocess
-        subprocess.run(["git", "submodule", "update", "--init"])
     unittest.main()
